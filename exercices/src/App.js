5d536d6e925578b3711d33d8a0db6502d6fc4017
rafaela-mansini <rafaela_mansini@hotmail.com>
2021-01-25 18:37:12 -0300

    Other ways to export a component

--- exercices/src/App.js
+++ exercices/src/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import  {Text} from 'react-native';
 
-function App(){
-    return <Text>Olá mundo...</Text>
-}
+// normal function
+// function App(){
+//     return <Text>Olá mundo...</Text>
+// }
 
-export default App;
+// save like a constant
+// const App = function() {
+//     return <Text>Olá mundo...</Text>
+// }
+
+// return like a arrow function
+// export default () => {
+//     return <Text>Return like a arrow function</Text>
+// }
+
+export default () => <Text>Return when is only one simple component</Text>
