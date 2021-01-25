99cdcc72753744e24d32f0ee184696815e207253
rafaela-mansini <rafaela_mansini@hotmail.com>
2021-01-25 19:49:28 -0300

    Styles

--- exercices/src/App.js
+++ exercices/src/App.js
@@ -3,9 +3,18 @@ import  { View, Text, StyleSheet } from 'react-native';
 import { Comp1, Comp2 } from './components/Components';
 
 export default () => (
-    <View>
+    <View style={style.App}>
         <Comp1 />
         <Comp2 />
     </View>
 
 )
+
+const style = StyleSheet.create({
+    App: {
+        backgroundColor: '#CCC',
+        flexGrow: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
+})
--- exercices/src/components/Components.js
+++ exercices/src/components/Components.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Text } from 'react-native'
+import Style from './style'
 
 export const Comp1 = () => {
-    return <Text>Componente 1</Text>
+    return <Text style={Style.textBig}>Componente 1</Text>
 }
 
 export const Comp2 = function () {
-    return <Text>Componente 1</Text>
+    return <Text>Componente 2</Text>
 }
 
 // can make this way too, but needs see a function or a const with a name
