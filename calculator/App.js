92b9a792e05ef2a8449fd9facd529137d54eef40
rafaela-mansini <rafaela_mansini@hotmail.com>
2021-02-02 20:04:00 -0300

    Styles from buttons

--- calculator/App.js
+++ calculator/App.js
@@ -3,32 +3,55 @@ import { StyleSheet, Text, View } from 'react-native'
 import Button from './src/components/Button'
 import Display from './src/components/Display'
 
+const initialState = {
+  displayValue: '0',
+  clearDisplay:  false,
+  operation: null,
+  values: [0, 0],
+  current: 0,
+}
+
 export default class App extends Component {
   state = {
     displayValue: '0'
   }
+
+  addDigit = n => {
+    this.setState({ displayValue: n})
+  }
+
+  clearMemory = () => {
+    this.setState({ displayValue: '0' })
+  }
+
+  setOperation = operation => {
+
+  }
+
   render(){
     return (
       <View style={styles.container}>
         <Display value={this.state.displayValue} />
         <View style={styles.buttons}>
-          <Button label="AC" />
-          <Button label="/" />
-          <Button label="7" />
-          <Button label="8" />
-          <Button label="9" />
-          <Button label="*" />
-          <Button label="4" />
-          <Button label="5" />
-          <Button label="6" />
-          <Button label="-" />
-          <Button label="1" />
-          <Button label="2" />
-          <Button label="3" />
-          <Button label="+" />
-          <Button label="0" />
-          <Button label="." />
-          <Button label="=" />
+          {/* Por não ter parametros o clearMemory pode ser chamado diretamente */}
+          <Button label="AC" triple onClick={this.clearMemory} /> 
+          {/* Por passar parametros, então precisa ser chamado uma arrow function, pois o padrão enviado é o evento do botão */}
+          <Button label="/" operation onClick={() => this.setOperation('/')} />
+          <Button label="7" onClick={() => this.addDigit(7)} />
+          <Button label="8" onClick={() => this.addDigit(8)} />
+          <Button label="9" onClick={() => this.addDigit(9)} />
+          <Button label="*" operation onClick={() => this.setOperation('*')} />
+          <Button label="4" onClick={() => this.addDigit(4)} />
+          <Button label="5" onClick={() => this.addDigit(5)} />
+          <Button label="6" onClick={() => this.addDigit(6)} />
+          <Button label="-" operation onClick={() => this.setOperation('-')} />
+          <Button label="1" onClick={() => this.addDigit(1)} />
+          <Button label="2" onClick={() => this.addDigit(2)} />
+          <Button label="3" onClick={() => this.addDigit(3)} />
+          <Button label="+" operation onClick={() => this.setOperation('+')} />
+          <Button label="0" double onClick={() => this.addDigit(0)} />
+          <Button label="." onClick={() => this.addDigit('.')} />
+          <Button label="=" operation onClick={() => this.setOperation('=')} />
         </View>
       </View>
     );
--- calculator/src/components/Button.js
+++ calculator/src/components/Button.js
@@ -2,9 +2,14 @@ import React from 'react'
 import { StyleSheet, Text, Dimensions, TouchableHighlight } from 'react-native'
 
 export default props => {
+    const stylesButton = [styles.button]
+    if(props.double) stylesButton.push(styles.buttonDouble)
+    if(props.triple) stylesButton.push(styles.buttonTriple)
+    if(props.operation) stylesButton.push(styles.operationButton)
+    
     return(
         <TouchableHighlight onPress={props.onClick}>
-            <Text style={styles.button}>{props.label}</Text>
+            <Text style={stylesButton}>{props.label}</Text>
         </TouchableHighlight>
     )
 }
@@ -17,7 +22,17 @@ const styles = StyleSheet.create({
         padding: 20,
         backgroundColor: '#f0f0f0',
         textAlign: 'center',
-        borderWidth: 1,
+        borderWidth: 0.5,
         borderColor: '#888'
+    },
+    operationButton: {
+        color: '#fff',
+        backgroundColor: '#fa8231',
+    },
+    buttonDouble: {
+        width: (Dimensions.get('window').width / 4) * 2,
+    },
+    buttonTriple: {
+        width: (Dimensions.get('window').width / 4) * 3,
     }
 })
