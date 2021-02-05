0ba695070104640d9e502cd4cdd21343974bdf55
rafaela-mansini <rafaela_mansini@hotmail.com>
2021-02-04 21:13:47 -0300

    Changin in functional function

--- calculator/App.js
+++ calculator/App.js
@@ -1,96 +1,97 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import Button from './src/components/Button'
 import Display from './src/components/Display'
 
-const initialState = {
-  displayValue: '0',
-  clearDisplay:  false,
-  operation: null,
-  values: [0, 0],
-  current: 0,
-}
+const App = () => {
+  const [displayValue, setDisplayValue] = useState('0')
+  const [clearDisplay, setClearDisplay] = useState(false)
+  const [operation, setOperation] = useState(null)
+  const [values, setValues] = useState([0, 0])
+  const [current, setCurrent] = useState(0)
 
-export default class App extends Component {
-  state = { ...initialState }
-
-  addDigit = n => {
-
-    const clearDisplay = this.state.displayValue === '0' || this.state.clearDisplay
-    
-    if (n === '.' && !clearDisplay && this.state.displayValue.includes('.')) return
-
-    const currentValue = clearDisplay ? '' : this.state.displayValue
-    const displayValue = currentValue + n
-    this.setState({ displayValue , clearDisplay: false })
-
-    if(n !== '.'){
-      const newValue = parseFloat(displayValue)
-      const values = [...this.state.values]
-      values[this.state.current] = newValue
-      this.setState({ values })
+  function addDigit(n) {
+    const isClearDisplay = displayValue === '0' || clearDisplay
+  
+    if(n === '.' && !isClearDisplay && displayValue.includes('.')) return
+  
+    const currentValue = isClearDisplay ? '' : displayValue
+    const newDisplayValue = currentValue + n
+    setDisplayValue(newDisplayValue)
+    setClearDisplay(false)
+  
+    if(n !== '.') {
+      const newValue = parseFloat(newDisplayValue)
+      const newValues = [...values]
+      newValues[current] = newValue
+      setValues(newValues)
     }
-
+  
   }
-
-  clearMemory = () => {
-    this.setState({...initialState})
+  
+  function clearMemory(){
+    setDisplayValue('0')
+    setClearDisplay(false)
+    setOperation(null)
+    setValues([0, 0])
+    setCurrent(0)
   }
-
-  setOperation = operation => {
-    if(this.state.current === 0){
-      this.setState({ operation, current: 1, clearDisplay: true })
+  
+  function saveOperation(op) {
+    if(current === 0){
+      setOperation(op)
+      setCurrent(1)
+      setClearDisplay(true)
     }
-    else{
-      const equals = operation === '='
-      const values = [...this.state.values]
-      
+    else {
+      const equals = op === '='
+      const newValues = [...values]
+      console.debug(newValues, 'in new values')
       try {
-        values[0] = eval(`${values[0]} ${this.state.operation} ${values[1]}`)
-      } catch (error) {
-        values[0] = this.state.values[0]
+        newValues[0] = eval(`${newValues[0]} ${operation} ${newValues[1]}`)
       }
-
-      values[1] = 0
-      this.setState({ 
-        displayValue: `${values[0]}`,
-        operation: equals ? null : operation,
-        current: equals ? 0 : 1,
-        clearDisplay: !equals,
-        values,
-      })
+      catch(error) {
+        newValues[0] = values[0]
+      }
+  
+      newValues[1] = 0
+      setDisplayValue(`${newValues[0]}`)
+      setOperation(equals ? null : op)
+      setCurrent(equals ? 0 : 1)
+      setClearDisplay(!equals)
+      setValues(newValues)
     }
   }
 
-  render(){
-    return (
-      <View style={styles.container}>
-        <Display value={this.state.displayValue} />
+  return (
+    <View style={styles.container}>
+        <Display value={displayValue} />
         <View style={styles.buttons}>
           {/* Por não ter parametros o clearMemory pode ser chamado diretamente */}
-          <Button label="AC" triple onClick={this.clearMemory} /> 
+          <Button label="AC" triple onClick={clearMemory} /> 
           {/* Por passar parametros, então precisa ser chamado uma arrow function, pois o padrão enviado é o evento do botão */}
-          <Button label="/" operation onClick={() => this.setOperation('/')} />
-          <Button label="7" onClick={() => this.addDigit(7)} />
-          <Button label="8" onClick={() => this.addDigit(8)} />
-          <Button label="9" onClick={() => this.addDigit(9)} />
-          <Button label="*" operation onClick={() => this.setOperation('*')} />
-          <Button label="4" onClick={() => this.addDigit(4)} />
-          <Button label="5" onClick={() => this.addDigit(5)} />
-          <Button label="6" onClick={() => this.addDigit(6)} />
-          <Button label="-" operation onClick={() => this.setOperation('-')} />
-          <Button label="1" onClick={() => this.addDigit(1)} />
-          <Button label="2" onClick={() => this.addDigit(2)} />
-          <Button label="3" onClick={() => this.addDigit(3)} />
-          <Button label="+" operation onClick={() => this.setOperation('+')} />
-          <Button label="0" double onClick={() => this.addDigit(0)} />
-          <Button label="." onClick={() => this.addDigit('.')} />
-          <Button label="=" operation onClick={() => this.setOperation('=')} />
+          <Button label="/" operation onClick={() => saveOperation('/')} />
+          <Button label="7" onClick={() => addDigit(7)} />
+          <Button label="8" onClick={() => addDigit(8)} />
+          <Button label="9" onClick={() => addDigit(9)} />
+          <Button label="*" operation onClick={() => saveOperation('*')} />
+          <Button label="4" onClick={() => addDigit(4)} />
+          <Button label="5" onClick={() => addDigit(5)} />
+          <Button label="6" onClick={() => addDigit(6)} />
+          <Button label="-" operation onClick={() => saveOperation('-')} />
+          <Button label="1" onClick={() => addDigit(1)} />
+          <Button label="2" onClick={() => addDigit(2)} />
+          <Button label="3" onClick={() => addDigit(3)} />
+          <Button label="+" operation onClick={() => saveOperation('+')} />
+          <Button label="0" double onClick={() => addDigit(0)} />
+          <Button label="." onClick={() => addDigit('.')} />
+          <Button label="=" operation onClick={() => saveOperation('=')} />
         </View>
       </View>
-    );
-  }
+  )
 }
+
+export default App
 
 const styles = StyleSheet.create({
   container: {
