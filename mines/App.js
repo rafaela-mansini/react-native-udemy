f59f7c19d07d7d01fe476d67c00bbdf7dc3564d4
rafaela-mansini <rafaela_mansini@hotmail.com>
2021-02-08 19:29:04 -0300

    Configuring params

--- mines/App.js
+++ mines/App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import params from './src/params'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Iniciando o mines</Text>
+      <Text>Tamanho da grade:
+        {params.getRowsAmount()} x {params.getColumnsAmount()}
+      </Text>
     </View>
   );
 }
