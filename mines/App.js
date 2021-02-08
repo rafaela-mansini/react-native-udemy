f1aaf36b27d688c3e352ea2483603c36f6044e8d
rafaela-mansini <rafaela_mansini@hotmail.com>
2021-02-08 19:36:10 -0300

    Block style

--- mines/App.js
+++ mines/App.js
@@ -1,7 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import params from './src/params'
+import Field from './src/components/Field'
 
 export default function App() {
   return (
@@ -10,6 +10,7 @@ export default function App() {
       <Text>Tamanho da grade:
         {params.getRowsAmount()} x {params.getColumnsAmount()}
       </Text>
+      <Field />
     </View>
   );
 }
